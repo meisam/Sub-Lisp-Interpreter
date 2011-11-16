96f45f8b13253728a044ebc28c4b20d1736000c7
fathi <fathi@cse.ohio-state.edu>
2011-11-15 21:48:00 -0500

    Smoke testcase for DEFUN primitive
    OPEN - task 23: Interpreter does not interpret s-Expressions correctly 
    http://github.com/meisam/Sub-Lisp-Interpreter/issues/issue/23

--- test/edu/osu/cse/meisam/interpreter/InterpreterSmokeTest.java
+++ test/edu/osu/cse/meisam/interpreter/InterpreterSmokeTest.java
@@ -196,6 +196,15 @@ public class InterpreterSmokeTest extends TestCase {
         System.out.println();
     }
 
+    public void testApplyFunExpresion() {
+        System.out.println("InterpreterSmokeTest.testDefunExpresion()");
+        final StringInputProvider in = new StringInputProvider(
+                "(Defun x (a b) (PLUS a 4)) (x 6 7)");
+        final Interpreter interpreter = new Interpreter(in, System.out);
+        interpreter.interpret();
+        System.out.println();
+    }
+
     public void FIXMEtestInterpret() { // FIXME
         final Interpreter interpreter = new Interpreter(
                 new InputStreamProvider(System.in), System.out);
